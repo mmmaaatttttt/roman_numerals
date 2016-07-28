b7875b915c179c4eef12302b9669f35453e343ce
Matt Lane <lane.matthew@gmail.com>
2016-07-28 14:22:34 -0700

    add solution

--- isValidRomanNumeral.js
+++ isValidRomanNumeral.js
@@ -1,5 +1,13 @@
 function isValidRomanNumeral(str) {
-
+  if (typeof str !== 'string' || !str) return false;
+  
+  // option 1
+  var regex = /^M?M?M?(CM|CD|D?C?C?C?)(XC|XL|L?X?X?X?)(IX|IV|V?I?I?I?)$/i;
+  
+  // option 2
+  // var regex = /^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/i;
+  
+  return regex.test(str);
 }
 
 module.exports = {
